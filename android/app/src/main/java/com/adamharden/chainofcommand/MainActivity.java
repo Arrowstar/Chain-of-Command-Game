1f3da9a71c34cebd9c1c60d3a87ca9e6fa178ec1
Arrowstar <adam.r.harden@gmail.com>
2026-05-23 09:44:08 -0500

    Update splash screen and app icon

--- android/app/src/main/java/com/adamharden/chainofcommand/MainActivity.java
+++ android/app/src/main/java/com/adamharden/chainofcommand/MainActivity.java
@@ -1,13 +1,11 @@
 package com.adamharden.chainofcommand;
 
 import android.os.Bundle;
-import androidx.core.splashscreen.SplashScreen;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        SplashScreen.installSplashScreen(this);
         super.onCreate(savedInstanceState);
     }
 }
