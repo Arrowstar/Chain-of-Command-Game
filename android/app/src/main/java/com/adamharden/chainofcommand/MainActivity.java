506eec11a5e59dc143a3fde7672711a1274ca498
Arrowstar <adam.r.harden@gmail.com>
2026-05-24 19:09:29 -0500

    Splash screen background is now navy blue

--- android/app/src/main/java/com/adamharden/chainofcommand/MainActivity.java
+++ android/app/src/main/java/com/adamharden/chainofcommand/MainActivity.java
@@ -2,10 +2,12 @@ package com.adamharden.chainofcommand;
 
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import androidx.core.splashscreen.SplashScreen;
 
 public class MainActivity extends BridgeActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.installSplashScreen(this);
         super.onCreate(savedInstanceState);
     }
 }
